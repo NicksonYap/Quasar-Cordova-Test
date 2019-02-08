e3671644505e9e34d4c2af3d86ff298f1185c8b4
Nickson Yap <hi@nickson.me>
2019-02-08 20:22:14 +0800

    Fix Android Barcode Scanner

--- src/pages/BarcodeScanner.vue
+++ src/pages/BarcodeScanner.vue
@@ -55,7 +55,8 @@ export default {
           prompt: 'Place a barcode inside the scan area', // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
           // formats : "QR_CODE,DATA_MATRIX,AZTEC", // default: all but PDF_417 and RSS_EXPANDED
-          formats : "QR_CODE,DATA_MATRIX,UPC_A,UPC_E,EAN_8,EAN_13,CODE_39,CODE_93,CODE_128,CODABAR,ITF,RSS14,PDF_417,RSS_EXPANDED,MSI,AZTEC",
+          // formats : "QR_CODE,DATA_MATRIX,UPC_A,UPC_E,EAN_8,EAN_13,CODE_39,CODE_93,CODE_128,CODABAR,ITF,RSS14,PDF_417,RSS_EXPANDED,MSI,AZTEC",  //some formats may confict
+          formats : "QR_CODE,DATA_MATRIX,EAN_13,PDF_417",
           // orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
           disableAnimations: true, // iOS
           disableSuccessBeep: false // iOS and Android
